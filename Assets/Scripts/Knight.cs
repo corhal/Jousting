764264fbdae290a40eca138dd35bdfba0072118f
Andrey <corhal@yandex.ru>
2018-04-16 09:23:54 +0300

    one of the knights moves now

---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.name != "LanceCollider" && Mathf.Approximately(other.attachedRigidbody.gravityScale, 0.0f)) {
+			other.attachedRigidbody.AddForce (Vector2.right * 350.0f);
+			other.attachedRigidbody.gravityScale = 1.0f;
+		}
+
+	}
+}
---
+++ Assets/Scripts/Rider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rider : MonoBehaviour {
+
+	public Knight Knight;
+
+	Rigidbody2D Rigidbody;
+	public float Speed;
+
+	void Awake () {
+		Rigidbody = GetComponent<Rigidbody2D> ();
+	}
+		
+	void Start () {
+		Rigidbody.velocity = Vector2.right * Speed;
+		Knight.gameObject.GetComponent <Rigidbody2D>().velocity = Vector2.right * Speed;
+	}	
+}
